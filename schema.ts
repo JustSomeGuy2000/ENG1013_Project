8873935f53f616ef19ce166a60121a287d12652b
Dr. Hazmat <placehold2608@gmail.com>
2026-04-17 15:35:36 +0800

    Added constructor and interface functions, tweaked the ticker system to make ticker functions return bool.

---
+++ schema.ts
@@ -0,0 +1,21 @@
+/**File for defining the types of component dictionaries since classes aren't allowed. */
+
+type int = number
+type float = number
+type PIN_CONSTANT = number
+
+type TrafficLight = {}
+
+type PedestrianLight = {}
+
+type WarningLight = {}
+
+type Floodlight = {}
+
+type UltrasonicSensor = {}
+
+type Buzzer = {}
+
+type DaylightSensor = {}
+
+type PushButton = {}
